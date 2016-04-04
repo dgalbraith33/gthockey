8a6e08d16c562bb0fd16887afb4d93652ddf89d0
Drew Galbraith <dgalbraith33@gmail.com>
2016-04-03 22:50:51 -0400

    Replaced default SECRET_KEY with a dummy

--- gtsite/settings.py
+++ gtsite/settings.py
@@ -20,12 +20,11 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.8/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'dar-b+8s+^o=0q#z5t8c%f)4m6&27rbq9wy9f&#5oekm&)&x_1'
+SECRET_KEY = 'DUMMY_SECRET_KEY'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = []
+DEBUG = False
+ALLOWED_HOSTS = ['.gthockey.com']
 
 
 # Application definition
