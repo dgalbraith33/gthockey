ed28f671b599b1d1304ce8f7fd1708e21c2147a7
Drew Galbraith <dgalbraith33@gmail.com>
2018-03-06 19:14:09 -0800

    Sort games on front page correctly

--- client/src/app/frontpage/frontpage.component.ts
+++ client/src/app/frontpage/frontpage.component.ts
@@ -26,25 +26,13 @@ export class FrontpageComponent implements OnInit {
   }
 
   private getGames() {
-    this.apiService.getGames({limit: 5, date_to: this.getYesterday(), desc: true}).subscribe(games => this.getRecent(games));
+    this.apiService.getGames({limit: 5, date_to: this.getYesterday(), desc: true}).subscribe(games => this.recentGames = games);
   }
 
   private getYesterday(): string {
     const date = new Date();
     date.setDate(date.getDate() - 1);
     return date.getFullYear() + '-' + (date.getMonth() + 1) + '-' + date.getDate();
-  }
-
-  private getRecent(games: GameMin[]) {
-    this.recentGames = [];
-    let index = games.length - 1;
-    const today = new Date();
-    while (this.recentGames.length < 5 && index >= 0) {
-      if (new Date(games[index].date) < today) {
-        this.recentGames.push(games[index]);
-      }
-      index--;
-    }
   }
 
 }
