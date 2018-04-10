b5aa67a1279a981c10da53b4bdf6579f68957471
Drew Galbraith <dgalbraith33@gmail.com>
2018-04-09 22:33:07 -0700

    Added new stuff

--- client/src/app/api/cart-item.ts
+++ client/src/app/api/cart-item.ts
@@ -9,4 +9,28 @@ export class CartItem {
     constructor(readonly shopItem: ShopItem) {
         this.item_id = this.shopItem.id;
     }
+
+    getOptions(): any[] {
+        const option_list = [];
+        for (const option_id of Object.keys(this.options)) {
+            for (const option of this.shopItem.options) {
+                if (option.id === Number.parseInt(option_id)) {
+                    option_list.push(option.display_name + ':' + this.options[option_id]);
+                }
+            }
+        }
+        return option_list;
+    }
+
+    getCustomOptions(): any[] {
+        const option_list = [];
+        for (const option_id of Object.keys(this.custom_options)) {
+            for (const option of this.shopItem.custom_options) {
+                if (option.id === Number.parseInt(option_id)) {
+                    option_list.push(option.display_name + ':' + this.custom_options[option_id]);
+                }
+            }
+        }
+        return option_list;
+    }
 }
