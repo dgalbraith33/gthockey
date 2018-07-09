9c2efe55b36596e89048a5d3d0617782cccbaace
Drew Galbraith <dgalbraith33@gmail.com>
2018-07-08 18:02:16 -0700

    Updated so Cart Nav only shows when cart has items

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -3,6 +3,7 @@ import { Component } from '@angular/core';
 import { Angulartics2GoogleAnalytics } from 'angulartics2/ga';
 
 import { environment } from './../environments/environment';
+import { CartService } from './cart/cart.service';
 
 @Component({
   selector: 'app-root',
@@ -11,5 +12,5 @@ import { environment } from './../environments/environment';
 })
 export class AppComponent {
 
-  constructor(angularitcs2GoogleAnalytics: Angulartics2GoogleAnalytics) {}
+  constructor(angularitcs2GoogleAnalytics: Angulartics2GoogleAnalytics, readonly cartService: CartService) {}
 }
